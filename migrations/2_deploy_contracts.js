7f014be0b544796639760bac10cb1dd107bbc7a0
ttl <talrasha007@gmail.com>
2018-07-20 19:54:00 +0800

    fix deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -21,10 +21,14 @@ module.exports = async function(deployer) {
     await deployer.deploy(Farm, coinCowCore.address);
     const farm = await Farm.deployed();
 
-    const testBtcCow = await deployer.deploy(TestCow, coinCowCore.address, farm.address, 'Test BTC Cow', 'TH/s', 'POW', 'BTC');
-    const testBchCow = await deployer.deploy(TestCow, coinCowCore.address, farm.address, 'Test BCH Cow', 'TH/s', 'POW', 'BCH');
-    const testEthCow = await deployer.deploy(TestCow, coinCowCore.address, farm.address, 'Test ETH Cow', 'TH/s', 'POW', 'ETH');
-    const testLamaCow = await deployer.deploy(TestCow, coinCowCore.address, farm.address, 'Test LAMA Cow', 'CCC', 'PLATFORM', 'ETH');
+    await deployer.deploy(TestCow, coinCowCore.address, farm.address, 'Test BTC Cow', 'TH/s', 'POW', 'BTC');
+    const testBtcCow = await TestCow.deployed();
+    await deployer.deploy(TestCow, coinCowCore.address, farm.address, 'Test BCH Cow', 'TH/s', 'POW', 'BCH');
+    const testBchCow = await TestCow.deployed();
+    await deployer.deploy(TestCow, coinCowCore.address, farm.address, 'Test ETH Cow', 'TH/s', 'POW', 'ETH');
+    const testEthCow = await TestCow.deployed();
+    await deployer.deploy(TestCow, coinCowCore.address, farm.address, 'Test LAMA Cow', 'CCC', 'PLATFORM', 'ETH');
+    const testLamaCow = await TestCow.deployed();
 
     await coinCowCore.registerCowInterface(testBtcCow.address);
     await coinCowCore.registerCowInterface(testBchCow.address);
